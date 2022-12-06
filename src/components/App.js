6d9cd2faf8cb422693b80583b33c3868f0021ec9
sara sallat <sallatsara@gmail.com>
2022-12-06 17:45:00 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [randomDog, setRandomDog] = useState(null);
+
+  useEffect(() => {
+    getRandomDog();
+  }, []);
+
+  const getRandomDog = async () => {
+    const request = await fetch("https://dog.ceo/api/breeds/image/random");
+    if (request.ok) {
+      const data = await request.json();
+      setIsLoading(false);
+      setRandomDog(data.message);
+    }
+  };
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={randomDog} alt="A Random Dog" />
+    </div>
+  );
+};
+
+export default App;
